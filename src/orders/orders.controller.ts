6f178e410a961843966b8b7fe501c29c959cafa0
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-12-22 15:50:31 -0600

    Add payment succeeded event pattern to OrdersController

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, NotImplementedException, ParseUUIDPipe } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
@@ -34,6 +34,13 @@ export class OrdersController {
   @MessagePattern('changeOrderStatus')
   changeOrderStatus(@Payload()changeOrderStatusDto: ChangeOrderStatusDto) {
   return this.ordersService.changeStatus(changeOrderStatusDto);
+  }
+
+  @EventPattern('payment.succeeded')
+  paidOrder(@Payload() paidOrderDto: any){
+    console.log({paidOrderDto});
+    return;
+
   }
 
   
